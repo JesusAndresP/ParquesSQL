d3f618320af10cd5da7d0712c53021b3d9e3f600
JesusAndresP <jeanpiro23@outlook.com>
2020-10-08 18:06:39 -0500

    Validación de campos en FormParques

--- src/app/components/form-parques/form-parques.component.ts
+++ src/app/components/form-parques/form-parques.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { ParquesService } from '../../services/parques.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { inject } from '@angular/core/testing';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { validateVerticalPosition } from '@angular/cdk/overlay';
 
 
 @Component({
@@ -10,23 +12,32 @@ import { inject } from '@angular/core/testing';
   styleUrls: ['./form-parques.component.scss']
 })
 export class FormParquesComponent implements OnInit {
-
+  signupForm: FormGroup
   constructor(
+    private _builder: FormBuilder,
     public parque: ParquesService,
     private dialogRef: MatDialogRef<FormParquesComponent>,
-    @Inject(MAT_DIALOG_DATA) data) { }
+    @Inject(MAT_DIALOG_DATA) data) {
+
+      this.signupForm =  this._builder.group({
+        ID_Parque:['',Validators.compose([Validators.min(1),Validators.required])],
+        Superficie_CA:['',Validators.compose([Validators.maxLength(10),Validators.required])],
+        Nombre_Parque:['',Validators.compose([Validators.maxLength(100),Validators.required])],
+        Fecha_Declaracion:['',Validators.required]
+      })
+     }
 
   ngOnInit(): void {
   }
 
-  onSaveForm(){
+  onSaveForm(values){
     if(this.parque.selected.id == null){
       let newParque = {
         ID_Parque: this.parque.selected.ID_Parque,
         Superficie_CA: this.parque.selected.Superficie_CA,
         Nombre_Parque: this.parque.selected.Nombre_Parque,
         Fecha_Declaracion: this.parque.selected.Fecha_Declaracion,
-        Activo: this.parque.selected.Activo
+        Activo: 'true'
       }
       this.parque.addParque(newParque);
     }
